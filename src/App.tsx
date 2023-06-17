ead344d4afe564e6cd5165a969b1a2d853045106
natsu727 <himajin0717@gmail.com>
2023-06-18 00:17:06 +0900

    rewrite App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import type { Component } from 'solid-js';
+import { Show, type Component, createSignal } from "solid-js";
+const { OpenAIClient, AzureKeyCredential } = await import("@azure/openai");
+const endpoint = "hiyashichuka.openai.azure.com";
+const azureApiKey = "cc6272d82b39473bb8b9a3e234988e30";
+const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+const deploymentId = "hiyashi";
+let prompt = ["こんにちわ"];
+const result = await client.getCompletions(deploymentId, prompt);
+for (const choice of result.choices) {
+  console.log(choice.text);
+}
 
-import logo from './logo.svg';
-import styles from './App.module.css';
-
+// let input: any;
+// let out: string = "テキストを入力してください";
+// const [Info, setInfo] = createSignal(false);
+// const [text, setText] = createSignal();
+// const writing = () => {
+//   setText(input.value);
+//   out_strs();
+// };
+// const out_strs = () => (out = String(text()));
+// const toggle = () => setInfo(!Info());
+// const sendText = (texts: string) => {
+//   // prompt = text;
+//   console.log(texts);
+//   toggle();
+// };
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <a>aaaaa</a>
+      {/* <div>
+        <input ref={input} oninput={writing} />
+        <button
+          onClick={(e) => {
+            if (!input.value.trim()) return;
+            sendText(input.value);
+            input.value = "";
+          }}
         >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+          Add
+        </button>
+      </div>
+      <h1>{out}</h1>
+      <Show when={Info()} fallback={""}>
+        <h1>ここに文字列</h1>
+      </Show> */}
+    </>
   );
 };
 
